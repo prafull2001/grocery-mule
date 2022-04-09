580133245892a634f286c8b745691bc820d41cd2
Prafull Sharma <prafull2001@gmail.com>
2022-04-09 18:05:49 -0500

    added providers

--- lib/screens/personal_list.dart
+++ lib/screens/personal_list.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_mule/components/rounded_ button.dart';
 import 'package:grocery_mule/constants.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:grocery_mule/providers/cowboy_provider.dart';
+import 'package:grocery_mule/providers/shopping_trip_provider.dart';
 
 class PersonalListScreen extends StatefulWidget {
   static String id = 'personallist_screen';
@@ -10,6 +15,13 @@ class PersonalListScreen extends StatefulWidget {
 }
 
 class _PersonalListScreen extends State<PersonalListScreen> {
+  String hostFirstName;
+
+  @override
+  void initState() {
+    hostFirstName = context.read<Cowboy>().firstName;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -19,6 +31,20 @@ class _PersonalListScreen extends State<PersonalListScreen> {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 30.0,
+              ),
+              Text(
+                  '$hostFirstName\'s List',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                  ),
+              ),
+            ]
+        ),
       ),
     );
   }
