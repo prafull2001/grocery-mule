c88a25340e9b74086f879d796642bc6c640411ed
Prafull Sharma <prafull2001@gmail.com>
2022-04-16 22:21:06 -0500

    removed extra code

--- lib/main.dart
+++ lib/main.dart
@@ -21,20 +21,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:after_layout/after_layout.dart';
 import 'dart:io';
 
-bool seen_intro;
+bool seen_intro; // global that updates with show_home's value upon startup
 
+// check if user has been shown the intro screen
 Future<Null> checkFirstSeen() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  bool cur_state = prefs.getBool('show_home') ?? false;
-  print('cur_state: ' + cur_state.toString());
+  bool cur_state = prefs.getBool('show_home') ?? false; // get value of cur_state, false if null
   if (cur_state == false) {
-    print('FIRST TIME LAUNCH');
     await prefs.setBool('show_home', true);
     seen_intro = false;
   } else if (cur_state == true ){
     seen_intro = true;
   } else {
-    print('something wrong with sharedpreference');
+    print('something wrong with show_home');
   }
 }
 
@@ -48,22 +47,19 @@ void main() async {
   Widget _defaultHome;
   await checkFirstSeen(); // update seen_intro bool
 
-  if (!seen_intro) {
+  if (!seen_intro) { // if user hasn't seen intro, show it
     _defaultHome = new IntroScreen();
   } else {
     final User curUser = FirebaseAuth.instance.currentUser;
     if(curUser == null) {
-      print('USER IS NULL');
-      //welcome screen
       _defaultHome = new WelcomeScreen();
     } else {
-      print('USER IS NOT NULL');
-      //listsscreen
       _defaultHome = new ListsScreen();
     }
   }
 
 
+  // pass entire MaterialApp as child of MultiProvider
   runApp(
       MultiProvider(
         providers: [
@@ -98,53 +94,3 @@ void main() async {
 }
 
 
-/*
-class GroceryMule extends StatefulWidget {
-  @override
-  _GroceryMuleState createState() => _GroceryMuleState();
-}
-//with AfterLayoutMixin<GroceryMule>
-class _GroceryMuleState extends State<GroceryMule> {
-
-  @override
-  void initState() {
-    super.initState();
-    checkFirstSeen();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //Widget home;
-    final User curUser = FirebaseAuth.instance.currentUser;
-
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: dark_beige,
-        scaffoldBackgroundColor: cream,
-        cardColor: dark_beige,
-        canvasColor: cream, colorScheme: ColorScheme.fromSwatch().copyWith(secondary: light_orange)
-      ),
-      home: home,
-      routes: {
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        RegistrationScreen.id: (context) => RegistrationScreen(),
-        ListsScreen.id: (context) => ListsScreen(),
-        CreateListScreen.id: (context) => CreateListScreen(true),
-        EditListScreen.id: (context) => EditListScreen(null),
-        UserInfoScreen.id: (context) => UserInfoScreen(),
-        ConfirmEmailScreen.id: (context) => ConfirmEmailScreen(),
-        FriendScreen.id: (context) => FriendScreen(),
-        PersonalListScreen.id: (context) => PersonalListScreen(),
-        CheckoutScreen.id: (context) => CheckoutScreen(),
-        IntroScreen.id: (context) => IntroScreen(),
-      },
-    );
-  }
-
-  // @override
-  // void afterFirstLayout(BuildContext context) {
-  //   checkFirstSeen();
-  // }
-}
-*/
